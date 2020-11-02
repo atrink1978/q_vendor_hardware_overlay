67f64aea65711739c84e6fcfbcd344919403b883
Ubuntu <ubuntu@xenial-aosp.lxd>
2020-11-02 17:49:17 +0100

    first commit

---
+++ overlay.mk
@@ -0,0 +1,18 @@
+PRODUCT_PACKAGES += \
+	HardwareOverlayPicker \
+	QtiAudio \
+	TrebleApp \
+	treble-overlay-NavBar \
+	treble-overlay-NightMode \
+	treble-overlay-SystemUI-FalseLocks \
+	treble-overlay-Telephony-LTE \
+	treble-overlay-caf-ims \
+	treble-overlay-devinputjack \
+	treble-overlay-gestures \
+	treble-overlay-hw-ims \
+	treble-overlay-mtk-ims \
+	treble-overlay-sec-ims \
+	treble-overlay-sprd-ims \
+	treble-overlay-webview \
+	treble-overlay-wifi5g \
+
